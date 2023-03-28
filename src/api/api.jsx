ac3d8d2af8a87b93b98805c967d96528c5f44883
Richard <wisemanrick@hotmail.co.uk>
2023-03-28 15:46:55 +0100

    Removed console logs

--- src/api/api.jsx
+++ src/api/api.jsx
@@ -9,17 +9,17 @@ const googleMapsAPI = axios.create({
       `keyword=food&location=${location.latitude}%2C${location.longitude}&radius=2000&type=restaurant&opennow=true&key=${GOOGLE_MAPS_APIKEY }`;
     
     return googleMapsAPI.get(path).then(({ data }) => {
-      //console.log(data);
+    
       return data.results;
     });
   };
-// API Call for Landmarks, needs keywords setting
+
   export const fetchAllPOI = (location, GOOGLE_MAPS_APIKEY ) => {
     let POIpath =
     `keyword=&location=${location.latitude}%2C${location.longitude}&radius=2000&type=tourist_attraction|park&key=${GOOGLE_MAPS_APIKEY }`;
-    // change path to be attractions
+    
     return googleMapsAPI.get(POIpath).then(({ data }) => {
-      console.log("POI",data);
+     
       return data.results;
     });
   };
