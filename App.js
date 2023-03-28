0179838b5a03da9d863bab3c1ff35fbe9cffad02
emmadenney <emma_denney@hotmail.com>
2023-03-28 16:44:26 +0100

    Route calculations and speed selector functionality

--- App.js
+++ App.js
@@ -5,20 +5,33 @@ import MapView, { Marker, PROVIDER_GOOGLE } from "react-native-maps";
 import { useState, useEffect } from "react";
 import * as Location from "expo-location";
 import Timer from "./src/Components/Timer";
+import SpeedSelector from "./src/Components/SpeedSelector";
 
 import PlotMarkers from "./src/Components/PlotMarkers";
 import DestinationSearch from "./src/Components/DestinationSearch";
 import PlotRoute from "./src/Components/PlotRoute";
+import RouteCalculations from "./src/Components/RouteCalculations";
 // import { GeocodeAddress } from "./Components/GeocodeAddress";
 
 export default function App() {
   const [location, setLocation] = useState();
   const [address, setAddress] = useState();
   const [searchedDestination, setSearchedDestination] = useState();
+  const [kmh, setKmh] = useState(4.5);
+  // ^^ kmh speed can be set by user in dropdown/slider form "slow", "medium", or "fast" each with a different kmh value. Ie. "medium" is 4.5kmh which is what this state is set as default. "slow" could be 3kmh, "fast" could be 6kmh
+  const [distances, setDistances] = useState([]);
+  const [markerWayPoints, setMarkerWayPoints] = useState([
+    { latitude: 53.47233724234388, longitude: -2.2386060301324466 },
+    { latitude: 53.477314035548645, longitude: -2.2359793198206 },
+    { latitude: 53.477392438907366, longitude: -2.2409457571161577 },
+    { latitude: 53.47233724234388, longitude: -2.2386060301324466 },
+  ]);
+  // ^^ markerWayPoints hardcoded for now
+
   // for directions
   //const origin = {latitude: 53.4721341, longitude: -2.2377251};// hard coded NC
   const origin = "Manchester Technology Centre";
-  const destination = { latitude: 53.636325899999996, longitude: -2.3278136 }; //Ricks house
+  const destination = "Manchester Technology Centre";
   const GOOGLE_MAPS_APIKEY = "AIzaSyDIt7GvEhgmT3io-pKMPqTKIif4jkx9-2U";
   // for directions
 
@@ -81,6 +94,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Timer></Timer>
+      <SpeedSelector setKmh={setKmh} />
       {location ? (
         /* DestinationSearch component creates a search function 
         above the map (only renders when there is a location set initially), 
@@ -106,6 +120,10 @@ export default function App() {
             origin={origin}
             destination={destination}
             GOOGLE_MAPS_APIKEY={GOOGLE_MAPS_APIKEY}
+            // setTotalDistance={setTotalDistance}
+            // setTotalWalkingDuration={setTotalWalkingDuration}
+            markerWayPoints={markerWayPoints}
+            setDistances={setDistances}
           />
           {searchedDestination ? (
             <Marker
@@ -120,6 +138,7 @@ export default function App() {
       ) : (
         <Text>Loading...</Text>
       )}
+      <RouteCalculations distances={distances} kmh={kmh} />
       <StatusBar style="auto" />
     </View>
   );
--- src/Components/PlotRoute.jsx
+++ src/Components/PlotRoute.jsx
@@ -1,15 +1,60 @@
 import MapViewDirections from "react-native-maps-directions";
+import { View, Text } from "react-native";
+import { useState, useEffect } from "react";
 
-export default function PlotRoute({ origin, destination, GOOGLE_MAPS_APIKEY }) {
+export default function PlotRoute({
+  origin,
+  destination,
+  GOOGLE_MAPS_APIKEY,
+  markerWayPoints,
+  setDistances,
+}) {
+  const [distanceA, setDistanceA] = useState(0);
+  const [distanceB, setDistanceB] = useState(0);
+  const [distanceC, setDistanceC] = useState(0);
+
+  useEffect(() => {
+    setDistances([distanceA, distanceB, distanceC]);
+  }, [distanceA, distanceB, distanceC]);
+
   return (
-    <MapViewDirections
-      origin={origin}
-      destination={destination}
-      mode="WALKING"
-      strokeWidth={5}
-      strokeColor="hotpink"
-      waypoints={["Trafford Centre"]}
-      apikey={GOOGLE_MAPS_APIKEY}
-    />
+    <>
+      <MapViewDirections
+        origin={markerWayPoints[0]}
+        destination={markerWayPoints[1]}
+        mode="WALKING"
+        strokeWidth={5}
+        strokeColor="hotpink"
+        // waypoints={markerWayPoints}
+        apikey={GOOGLE_MAPS_APIKEY}
+        onReady={(result) => {
+          setDistanceA(result.distance);
+        }}
+      />
+      <MapViewDirections
+        origin={markerWayPoints[1]}
+        destination={markerWayPoints[2]}
+        mode="WALKING"
+        strokeWidth={5}
+        strokeColor="blue"
+        // waypoints={markerWayPoints}
+        apikey={GOOGLE_MAPS_APIKEY}
+        onReady={(result) => {
+          setDistanceB(result.distance);
+        }}
+      />
+      <MapViewDirections
+        origin={markerWayPoints[2]}
+        destination={markerWayPoints[3]}
+        mode="WALKING"
+        strokeWidth={5}
+        strokeColor="orange"
+        // waypoints={markerWayPoints}
+        apikey={GOOGLE_MAPS_APIKEY}
+        onReady={(result) => {
+          setDistanceC(result.distance);
+        }}
+      />
+    </>
   );
 }
