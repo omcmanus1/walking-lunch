47682b53aaf2e678b8d7550cc809b8939193b92b
emmadenney <emma_denney@hotmail.com>
2023-03-29 11:10:44 +0100

    Added notification warnings if journey time is longer than 45 mins or an hour

--- src/Components/PlotRoute.jsx
+++ src/Components/PlotRoute.jsx
@@ -5,6 +5,8 @@ export default function PlotRoute({
   GOOGLE_MAPS_APIKEY,
   markerLocations,
   setDistances,
+  testLocations,
+  showRoute,
 }) {
   const [distanceA, setDistanceA] = useState(0);
   const [distanceB, setDistanceB] = useState(0);
@@ -14,47 +16,89 @@ export default function PlotRoute({
     setDistances([distanceA, distanceB, distanceC]);
   }, [distanceA, distanceB, distanceC]);
 
-  console.log("array---------->", markerLocations);
+  // console.log("array---------->", markerLocations);
   // THIS COMPONENT WON'T WORK CURRENTLY BECAUSE MARKERLOCATIONS ARRAY IS EMPTY/UNDEFINED BASED ON HOW PLOTMARKERS IS CURRENTLY SET UP TO ONLY PLOT LOCATIONS FOR A SEARCHED DESTINATION
 
   return (
     <>
-      <MapViewDirections
-        origin={markerLocations[0].coordinate}
-        destination={markerLocations[1].coordinate}
-        mode="WALKING"
-        strokeWidth={5}
-        strokeColor="hotpink"
-        // waypoints={markerWayPoints}
-        apikey={GOOGLE_MAPS_APIKEY}
-        onReady={(result) => {
-          setDistanceA(result.distance);
-        }}
-      />
-      <MapViewDirections
-        origin={markerLocations[1].coordinate}
-        destination={markerLocations[2].coordinate}
-        mode="WALKING"
-        strokeWidth={5}
-        strokeColor="blue"
-        // waypoints={markerWayPoints}
-        apikey={GOOGLE_MAPS_APIKEY}
-        onReady={(result) => {
-          setDistanceB(result.distance);
-        }}
-      />
-      <MapViewDirections
-        origin={markerLocations[2].coordinate}
-        destination={markerLocations[3].coordinate}
-        mode="WALKING"
-        strokeWidth={5}
-        strokeColor="orange"
-        // waypoints={markerWayPoints}
-        apikey={GOOGLE_MAPS_APIKEY}
-        onReady={(result) => {
-          setDistanceC(result.distance);
-        }}
-      />
+      {showRoute ? (
+        <>
+          <MapViewDirections
+            origin={testLocations[0]}
+            destination={testLocations[1]}
+            mode="WALKING"
+            strokeWidth={5}
+            strokeColor="hotpink"
+            // waypoints={markerWayPoints}
+            apikey={GOOGLE_MAPS_APIKEY}
+            onReady={(result) => {
+              setDistanceA(result.distance);
+            }}
+          />
+          <MapViewDirections
+            origin={testLocations[1]}
+            destination={testLocations[2]}
+            mode="WALKING"
+            strokeWidth={5}
+            strokeColor="blue"
+            // waypoints={markerWayPoints}
+            apikey={GOOGLE_MAPS_APIKEY}
+            onReady={(result) => {
+              setDistanceB(result.distance);
+            }}
+          />
+          <MapViewDirections
+            origin={testLocations[2]}
+            destination={testLocations[3]}
+            mode="WALKING"
+            strokeWidth={5}
+            strokeColor="orange"
+            // waypoints={markerWayPoints}
+            apikey={GOOGLE_MAPS_APIKEY}
+            onReady={(result) => {
+              setDistanceC(result.distance);
+            }}
+          />
+        </>
+      ) : null}
+      <>
+        <MapViewDirections
+          origin={testLocations[0]}
+          destination={testLocations[1]}
+          mode="WALKING"
+          strokeWidth={5}
+          strokeColor="hotpink"
+          // waypoints={markerWayPoints}
+          apikey={GOOGLE_MAPS_APIKEY}
+          onReady={(result) => {
+            setDistanceA(result.distance);
+          }}
+        />
+        <MapViewDirections
+          origin={testLocations[1]}
+          destination={testLocations[2]}
+          mode="WALKING"
+          strokeWidth={5}
+          strokeColor="blue"
+          // waypoints={markerWayPoints}
+          apikey={GOOGLE_MAPS_APIKEY}
+          onReady={(result) => {
+            setDistanceB(result.distance);
+          }}
+        />
+        <MapViewDirections
+          origin={testLocations[2]}
+          destination={testLocations[3]}
+          mode="WALKING"
+          strokeWidth={5}
+          strokeColor="orange"
+          // waypoints={markerWayPoints}
+          apikey={GOOGLE_MAPS_APIKEY}
+          onReady={(result) => {
+            setDistanceC(result.distance);
+          }}
+        />
+      </>
     </>
   );
 }
