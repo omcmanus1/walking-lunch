f1660e293d901b2d505cc0d4d403399849e23d8f
Richard <wisemanrick@hotmail.co.uk>
2023-03-31 13:34:58 +0100

    Amend basic navigation

--- src/Components/NavigatorStack.jsx
+++ src/Components/NavigatorStack.jsx
@@ -1,16 +1,16 @@
-import React from "react";
+// import React from "react";
 
-import { createStackNavigator } from "@react-navigation/stack";
-import StartTimer from "./StartTimer";
+// import { createStackNavigator } from "@react-navigation/stack";
+// import StartTimer from "./StartTimer";
 
 
 
-const Stack = createStackNavigator();
+// const Stack = createStackNavigator();
 
-export const RouteMapView = () => {
-  return (
-    <Stack.Navigator>
-      <Stack.Screen name="Start Walk" component={StartTimer} />
-    </Stack.Navigator>
-  );
-}
+// export const RouteMapView = () => {
+//   return (
+//     <Stack.Navigator>
+//       <Stack.Screen name="Start Walk" component={StartTimer} />
+//     </Stack.Navigator>
+//   );
+// }
